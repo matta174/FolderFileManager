36f1802ea7e6bc639100173c5e69058f4e5ecebf
Matthew Compton <matthewc174@gmail.com>
2019-12-16 18:37:32 -0500

    front end dev
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- src/GUI.py
+++ src/GUI.py
@@ -1,24 +1,36 @@
 import json
 
-from tkinter import Tk, Label, Button
-class MyFirstGUI:
+from tkinter import Tk, Label, Button, filedialog, Text, StringVar
+class FolderFileManager:
     def __init__(self, master):
         self.master = master
         master.title("Matt's Big Adventure")
+        master.geometry('500x500')
 
         self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
+        self.label.grid()
 
-        self.greet_button = Button(master, text="Greet", command=self.update_configuration)
-        self.greet_button.pack()
+        self.folder_button = Button(master, text="Select folder to watch", command=self.select_watch_folder)
+        self.folder_button.grid(row=0,column=0)
+        
+        self.v = StringVar()
+        self.folder_text =  Label(master,textvariable=self.v)
 
+        
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
 
-    def greet(self):
-        print("Greetings!")
 
-    def update_configuration(self):
+        self.close_button.grid()
+        self.folder_button.grid()
+
+
+
+    def select_watch_folder(self):
+        folder_to_watch = filedialog.askdirectory()
+        self.v.set(folder_to_watch)
+        self.update_configuration(folder_to_watch)
+
+    def update_configuration(self,sourcefolder):
         print("Updating Configuration")
         jsonFile = open("config.json", "r") # Open the JSON file for reading
         data = json.load(jsonFile) # Read the JSON into the buffer
@@ -26,8 +38,8 @@ class MyFirstGUI:
 
         ## Working with buffered content
         tmp = data["source_folders"] 
-        data["source_folders"] = 'C:/notreal'
-        data["mode"] = "replay"
+        data["source_folders"] = sourcefolder
+        #data["mode"] = "replay"
 
         ## Save our changes to JSON file
         jsonFile = open("config.json", "w+")
@@ -35,5 +47,5 @@ class MyFirstGUI:
         jsonFile.close()
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+my_gui = FolderFileManager(root)
 root.mainloop()
