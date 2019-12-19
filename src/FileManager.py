5b698b96a180ba5160487d2606f3ae41b040a57a
Matthew Compton <matthewc174@gmail.com>
2019-12-18 20:44:32 -0500

    updates
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- src/FileManager.py
+++ src/FileManager.py
@@ -13,13 +13,12 @@ from watchdog.events import PatternMatchingEventHandler
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-source_folders = data['source_folders']
-target_folders = data['target_folders']
+source_folder = data['source_folder']
 picture_folder = data['pictures_folder']
 music_folder = data['music_folder']
 
 class Utils(PatternMatchingEventHandler):
-    
+
     def on_any_event(self,event):
         print('anything' + str(event))
 
@@ -40,11 +39,14 @@ class Utils(PatternMatchingEventHandler):
 
     def on_created(self,event):
         src_path = event.src_path
-        # filename, file_extension = os.path.splitext(src_path)
+        
         full_filename = os.path.basename(src_path)
-        filename,file_extension= os.path.splitext(full_filename) # ntpath.basename(event.src_path)
+        filename,file_extension= os.path.splitext(full_filename) 
         new_full_filename = folder_destination + "/" + full_filename
 
+        image_dict = ['.img','.jpg','.jpeg','.png','.apng','.bmp','.gif','.ico','.cur','.jfif','.pjpeg','.pjp','.svg','.tif','.tiff','.webp']
+        sound_dict = ['.3gp','.aa','.aac','.aax','.act','.aiff','.alac','.amr','.ape','.au','.awb','.dct','.dss','.dvf','.flac','.gsm','.iklax','.ivs','.m4a','.m4b','.m4p','.mmf','.mp3','.mpc','.msv','.nmf','.nsf','.ogg','.oga','.mogg','.opus','.raw','.sln','.tta','.voc','.vox','.wav','.wma','.wv','.webm','.8svx']
+        document_dict = ['.doc','.docx','.xls','.xlsx','.pdf','.html','.txt','.pptx','.ppt','.pps','.odp']
         if (file_extension == '.txt'):
             try:
                 os.rename(src_path, new_full_filename)
@@ -56,17 +58,25 @@ class Utils(PatternMatchingEventHandler):
                 shutil.move(src_path,new_full_filename)
             except Exception as e: print(e)
 
-        elif (file_extension == '.jpg' or file_extension == '.png'):
+        elif (file_extension in image_dict):
             new_destination = picture_folder
             new_full_filename = picture_folder + '/' + full_filename
             try:
                 shutil.move(src_path,new_full_filename)
             except Exception as e: print(e)
         
+    # def on_modified(self,event):
+    #     for filename in os.listdir(folder_to_track):
+    #         src = folder_to_track + "/" + filename
+    #         new_destination = folder_destination + "/" + filename
+    #         try:
+    #             os.rename(src,new_destination)
+    #         except:
+    #             print('uh oh ')
+    
 
-
-folder_to_track = source_folders
-folder_destination = target_folders
+folder_to_track = source_folder
+folder_destination = ""
 
 event_handler = Utils()
 observer = Observer()
--- src/GUI.py
+++ src/GUI.py
@@ -1,50 +1,101 @@
 import json
-
+import tkinter as tk
 from tkinter import Tk, Label, Button, filedialog, Text, StringVar, dialog
-class FolderFileManager:
+class FolderFileManager(tk.Frame):
 
     def __init__(self, master):
         self.master = master
-        master.title("Folder Shepherd")
-        master.geometry('300x300')
+        tk.Frame.__init__(self,self.master)
+        self.configure_gui()
 
+        # Source Folder Section
 
-        self.label = Label(master,text="Source folder: ")
+        self.label = Label(master,text="Source Folder: ")
+        self.label.grid(row=4,column=1)
+
+        self.v = StringVar(value=self.source_folder)
+
         self.source_folder_button = Button(master, text="Select folder to watch", command=self.select_watch_folder)
+        self.source_folder_button.grid(row=4,column=0,padx=10)
 
-        self.label2 = Label(master,text="Target folder: ")
-        self.target_folder_button = Button(master,text="Select folder to place files", command=self.select_target_folder)
-        
-        self.v = StringVar()
         self.folder_text =  Label(master,textvariable=self.v)
+        self.folder_text.grid(row=4,column=2)
 
-        self.v2 = StringVar()
-        self.target_folder_text = Label(master,textvariable=self.v2)
+        # Image Folder Section
+        self.label2 = Label(master,text="Image Folder: ")
+        self.label2.grid(row=7,column=1,pady=10)
+
+        self.pictures_folder_button = Button(master,text="Select folder to place images", command=self.select_picture_folder)
+        self.pictures_folder_button.grid(row=7,column=0,padx=10)
+
+        self.v2 = StringVar(value=self.picture_folder)
         
+        self.target_picture_text = Label(master,textvariable=self.v2)
+        self.target_picture_text.grid(row=7,column=2,pady=10)
+
+        # Music Folder Section
+        self.label3 = Label(master,text="Music Folder: ")
+        self.label3.grid(row=8,column=1,pady=10)
+
+        self.music_folder_button = Button(master,text="Select folder to place music files", command=self.select_music_folder)
+        self.music_folder_button.grid(row=8,column=0,padx=10)
+
+
+        self.v3 = StringVar(value=self.music_folder)
+
+        self.target_music_text = Label(master,textvariable=self.v3)
+        self.target_music_text.grid(row=8,column=2,pady=10)
+
+        # Documents Folder Section
+        self.label4 = Label(master,text="Documents Folder: ")
+        self.label4.grid(row=9,column=1,pady=10)
+
+        self.documents_folder_button = Button(master,text="Select folder to place document files", command=self.select_documents_folder)
+        self.documents_folder_button.grid(row=9,column=0,padx=10)
+
+
+        self.v4 = StringVar(value=self.documents_folder)
+
+        self.target_documents_text = Label(master,textvariable=self.v4)
+        self.target_documents_text.grid(row=9,column=2,pady=10)
+
         
         self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.grid(row=10,column=2)
 
         self.save_button = Button(master,text='Save', command=self.update_configuration)
-
-        self.source_folder_button.grid(row=1,column=2)
-        self.folder_text.grid(row=4,column=2)
-        self.target_folder_text.grid(row=5,column=2)
-        self.label.grid(row=4,column=1)
-        self.label2.grid(row=5,column=1)
-        self.save_button.grid(row=6,column=2)
-        self.close_button.grid(row=7,column=2)
-        self.target_folder_button.grid(row=2,column=2)
-        # self.source_folder_button.grid()
+        self.save_button.grid(row=10,column=1)
 
 
+    def configure_gui(self):
+        self.master.title("Folder Shepherd")
+        self.master.geometry("")
+        self.master.resizable(False,False)
+        self.master.iconbitmap(r"src\\images\\fileshepherd.ico")
+        
+
+        with open('config.json') as config_file:
+            data = json.load(config_file)
+        self.source_folder = data['source_folder']
+        self.documents_folder = data['documents_folder']
+        self.picture_folder = data['pictures_folder']
+        self.music_folder = data['music_folder']
 
     def select_watch_folder(self):
         sourcefolder = filedialog.askdirectory()
         self.v.set(sourcefolder)
-        # self.update_configuration(folder_to_watch)
-    def select_target_folder(self):
+
+    def select_picture_folder(self):
         targetfolder = filedialog.askdirectory()
         self.v2.set(targetfolder)
+
+    def select_music_folder(self):
+        targetfolder = filedialog.askdirectory()
+        self.v3.set(targetfolder)
+
+    def select_documents_folder(self):
+        targetfolder = filedialog.askdirectory()
+        self.v4.set(targetfolder)
 
     def update_configuration(self):
         print("Updating Configuration")
@@ -53,9 +104,11 @@ class FolderFileManager:
         jsonFile.close() # Close the JSON file
 
         ## Working with buffered content
-        tmp = data["source_folders"] 
-        data["source_folders"] = self.v.get()
-        data["target_folders"] = self.v2.get()
+        tmp = data["source_folder"] 
+        data["source_folder"] = self.v.get()
+        data["pictures_folder"] = self.v2.get()
+        data['music_folder'] = self.v3.get()
+        data['documents_folder'] = self.v4.get()
         #data["mode"] = "replay"
 
         ## Save our changes to JSON file
@@ -63,6 +116,8 @@ class FolderFileManager:
         jsonFile.write(json.dumps(data))
         jsonFile.close()
 
-root = Tk()
-my_gui = FolderFileManager(root)
-root.mainloop()
+
+if __name__ == '__main__':
+   root = tk.Tk()
+   main_app =  FolderFileManager(root)
+   root.mainloop()
